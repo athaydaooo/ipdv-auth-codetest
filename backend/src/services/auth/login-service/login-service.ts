bb6f6a18572cab27bbb05f8c8cafcdc931543e16
Lucas Athayde <athaydaooo@gmail.com>
2025-06-23 16:22:32 -0300

    feat: added auth services implementation

--- backend/src/services/auth/login-service/login-service.ts
+++ backend/src/services/auth/login-service/login-service.ts
@@ -1,50 +1,44 @@
-import configs from "@configs/index";
 import { authInvalidCredentials } from "@errors/auth";
-import { parseDuration } from "@helpers/parse-duration";
 import { Session, User } from "@prisma/client";
 import { SessionRepository } from "@repositories/session-repository";
 import { UserRepository } from "@repositories/user-repository";
 import { crypto } from "@utils/crypto";
 import { jwtAuth } from "@utils/jwt";
-import { JwtPayload } from "../../../types/auth/jwt-payload";
 
 interface LoginServiceResponse { user: User; session: Session }
 
 export class LoginService {
-    async execute(email: string, password: string): Promise<LoginServiceResponse> {
-        const userRepository = new UserRepository();
-        const sessionRepository = new SessionRepository();
+    private userRepository: UserRepository;
+    private sessionRepository: SessionRepository;
 
-        const user = await userRepository.getUserByEmail(email);
+    constructor(userRepository: UserRepository, sessionRepository: SessionRepository) {
+        this.userRepository = userRepository;
+        this.sessionRepository = sessionRepository;
+    }
+
+    /**
+     * Logs in a user by validating their email and password, and creating a session.
+     * @param email - The user's email address.
+     * @param password - The user's password.
+     * @returns A promise that resolves to an object containing the user and session information.
+     * @throws authInvalidCredentials if the email or password is incorrect.
+     */
+    async execute(email: string, password: string): Promise<LoginServiceResponse> {
+        const user = await this.userRepository.getUserByEmail(email);
         if (!user) { throw authInvalidCredentials; }
 
-        const decryptedPassword = await crypto.decrypt({ data: password });
-        if (decryptedPassword !== password) { throw authInvalidCredentials; }
+        const decryptedPassword = crypto.decrypt({ data: user.password });
+        if (password !== decryptedPassword) 
+         throw authInvalidCredentials;
 
-        const jwtPayload = {
-            userId: user.id,
-            name: user.name,
-            email: user.email,
-            role: [], 
-        } as JwtPayload; 
+        const roles = (await this.userRepository.getRolesByUser(user.id)).map(role => role.name);
+        const sessionData = jwtAuth.generateSessionByUser(user, roles);
 
-        const roles = await userRepository.getRolesByUser(user.id);
-        jwtPayload.role = roles.map(role => role.name);
-
-        const sesionToken = await sessionRepository.createSession({
-            userId: user.id,
-            accessToken: jwtAuth.generateToken(jwtPayload),
-            refreshToken: jwtAuth.generateToken(jwtPayload, configs.jwtRefreshExpiration),
-            accessTokenExpiresAt: new Date(Date.now() + parseDuration(configs.jwtExpiration)),
-            refreshTokenExpiresAt: new Date(Date.now() + parseDuration(configs.jwtRefreshExpiration)),
-            isRevoked: false,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        });
+        const session = await this.sessionRepository.createSession(sessionData);
 
         return {
             user,
-            session: sesionToken,
+            session,
         };
     }
 }
--- backend/src/services/auth/logout-service/logout-service.ts
+++ backend/src/services/auth/logout-service/logout-service.ts
@@ -1,8 +1,24 @@
+import { authAlreadyRevokedSession, authSessionNotFound } from "@errors/auth";
+import { SessionRepository } from "@repositories/session-repository";
 
 export class LogoutService {
+    private sessionRepository: SessionRepository;
+
+    constructor(sessionRepository: SessionRepository) {
+        this.sessionRepository = sessionRepository;
+    }
+    
+    /**
+     * Logs out the user by revoking their session using the provided access token.
+     * @param accessToken - The access token of the session to be revoked.
+     * @throws authSessionNotFound if the session with the provided access token does not exist.
+     */
     async execute(accessToken: string): Promise<void> {
-        // This method should contain the logic for user login
-        // Placeholder implementation to satisfy return type
-        throw new Error("Method not implemented.");
+        const session = await this.sessionRepository.getSessionByAccessToken(accessToken);
+        if (!session) { throw authSessionNotFound; }
+
+        if (session.isRevoked) { throw authAlreadyRevokedSession;}
+
+        await this.sessionRepository.revokeSession(accessToken);
     }
 }
